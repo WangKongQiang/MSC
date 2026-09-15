ecde9c650867d23dd4c346d8fbfca0237309c4ae
WangKongQiang <wangkongqiang60@gmail.com>
2026-09-15 18:49:49 +0800

    Delete MSC directory

--- MSC/datasets/Paraphrase_French/examples/translation_moe/translation_moe_src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright https://www.ynu.edu.cn/ or its affiliates school. All Rights Reserved
-
-Author: Kongqiang Wang (wangkongqiang60@gmail.com)
-Date: 05/27/2026
-"""
-
-from . import translation_moe  # noqa
--- MSC/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Copyright https://www.ynu.edu.cn/ or its affiliated school. All Rights Reserved
-
-Author: Kongqiang Wang (wangkongqiang60@gmail.com)
-Date: 05/27/2026
-"""
